2e7b9de6e48527c6cd4abb6fc3e769d947e60094
Ryukudz <39871872+Ryukudz@users.noreply.github.com>
2023-07-27 16:00:45 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# rumbot
-Rumble video viewer bot tool, sends viewers to the livestream without relying on proxies or going hard on your hardware resources.
+# Rumbot
+
+simple, easy to use rumble viewbot.
+
+## Installation
+
+```sh
+git clone https://github.com/Ryukudz/rumbt
+cd rumbot
+pip install -r requirements.txt
+```
+
+## Usage
+
+```sh
+python3 rumbot.py
+```
+
+## How to find the video_id
+
+Rumble does not provide any API or a functionality that allows me
+to fetch video id using the channel name so you will have to do it this way:
+
+```
+- Go to your live livestream
+- Click F12 or right click > Inspect
+- Click on the network tab and look for the GET request to wn0.rumble.com
+```
+
+![TUTO](https://raw.githubusercontent.com/Ryukudz/rumbot/main/help.png)
+
+## Preview 🧙‍♂️
+
+![Preview](https://raw.githubusercontent.com/Ryukudz/Rumble-Viewer-Bot/main/preview.gif)
+Video Presentation: https://www.youtube.com/watch?v=7DvaqMbTFFk
+
+# License
+
+`rumbot` is made with by [Ryuku](https://github.com/Ryukudz/).
---
+++ rumbot.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+"""
+Author: Ryuku - https://ryukudz.com
+"""
+import httpx
+import random
+import string
+import asyncio
+import webbrowser
+import tkinter as tk
+
+channel_id_entry = None
+num_viewers_entry = None
+status_label = None
+send_button = None
+stop_button = None
+are_we_botting = False
+
+def create_gui():
+    global channel_id_entry, num_viewers_entry, status_label, send_button, stop_button
+    app = tk.Tk()
+    app.title("Rumbot - Rumble Viewbot tool")
+    app.configure(bg="#161618")
+
+    channel_id_label = tk.Label(app, text="Video ID:", fg="white", bg="#161618")
+    channel_id_label.grid(row=0, column=0, padx=10, pady=5)
+    channel_id_entry = tk.Entry(app, width=20)
+    channel_id_entry.grid(row=0, column=1, padx=10, pady=5)
+
+    num_viewers_label = tk.Label(app, text="Number of Viewers:", fg="white", bg="#161618")
+    num_viewers_label.grid(row=1, column=0, padx=10, pady=5)
+    num_viewers_entry = tk.Entry(app, width=20)
+    num_viewers_entry.grid(row=1, column=1, padx=10, pady=5)
+
+    question_label = tk.Label(app, text="?", fg="white", bg="#0078d4", cursor="hand2")
+    question_label.grid(row=0, column=0, padx=0, pady=0, sticky="e")
+    question_label.bind("<Button-1>", lambda e: on_help_click())
+
+    status_label = tk.Label(app, text="", fg="green", bg="#161618")
+    status_label.grid(row=3, column=0, columnspan=2, padx=10, pady=5)
+
+    send_button = tk.Button(app, text="Send", width=12, command=on_send_clicked, bg="#0078d4", fg="white", borderwidth=0)
+    send_button.grid(row=4, column=0, padx=10, pady=5)
+
+    stop_button = tk.Button(app, text="Stop", width=12, command=on_stop_clicked, bg="#e81123", fg="white", borderwidth=0)
+    stop_button.grid(row=4, column=1, padx=10, pady=5)
+
+    button_font = ("Arial", 12, "bold")
+    send_button.config(font=button_font)
+    stop_button.config(font=button_font)
+    footer_label = tk.Label(app, text="© - Ryuku", fg="white", bg="#161618", cursor="hand2")
+    footer_label.grid(row=5, column=1, pady=5, padx=10, sticky="e")
+    footer_label.bind("<Button-1>", lambda e: on_footer_click())
+
+    app.mainloop()
+
+def on_help_click():
+    webbrowser.open("https://raw.githubusercontent.com/Ryukudz/Rumble-Viewer-Bot/main/tab.png")
+
+def on_footer_click():
+    webbrowser.open("https://ryukudz.com")
+
+def generate_viewer_id():
+    characters = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(characters) for _ in range(8))
+
+
+async def start_viewbotting(session, channel_id, num_viewers):
+    url = f"https://wn0.rumble.com/service.php?video_id={channel_id}&name=video.watching-now"
+    response = await session.get(url)
+    if "data" not in response.json():
+        status_label.config(text="Channel doesn't exist.", fg="red")
+        return False
+
+
+    url = "https://wn0.rumble.com/service.php?name=video.watching-now"
+    tasks = []
+    for _ in range(num_viewers):
+        viewer_id = generate_viewer_id()
+        data = {"video_id": channel_id, "viewer_id": viewer_id}
+        task = session.post(url, data=data)
+        tasks.append(task)
+
+    await asyncio.gather(*tasks)
+    channel_id_entry.config(state=tk.DISABLED)
+    num_viewers_entry.config(state=tk.DISABLED)
+    send_button.config(state=tk.DISABLED)
+
+    return True
+
+
+async def keep_viewbotting(session, channel_id, num_viewers, interval_seconds=80):
+    while True:
+        if await start_viewbotting(session, channel_id, num_viewers):
+            await asyncio.sleep(interval_seconds)
+        else:
+            break
+        await session.aclose()
+
+
+def on_send_clicked():
+    global are_we_botting
+    channel_id = channel_id_entry.get()
+    num_viewers = num_viewers_entry.get()
+
+    if not channel_id or not num_viewers:
+        status_label.config(text="Please enter both fields.", fg="red")
+        return
+
+    num_viewers = int(num_viewers)
+    status_label.config(text="Viewers sent...", fg="green")
+    session = httpx.AsyncClient()
+    asyncio.run(start_viewbotting(session, channel_id, num_viewers))
+    are_we_botting = True
+
+
+def on_stop_clicked():
+    global are_we_botting
+    if are_we_botting:
+      status_label.config(text="Stopping...", fg="red")
+      send_button.config(state=tk.NORMAL)
+      channel_id_entry.config(state=tk.NORMAL)
+      num_viewers_entry.config(state=tk.NORMAL)
+      are_we_botting = False
+    else:
+      status_label.config(text="Nothing to stop...", fg="red")
+
+
+if __name__ == "__main__":
+    create_gui()
